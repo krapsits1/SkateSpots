bc93c027dbbec0355e5e185afeaa39e58ef10f92
Emils Vetra <emilsvetra@gmail.com>
2025-02-27 17:29:19 +0200

    favicon and og meta tags

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Skate Spots</title>
+<title>SkatesSpots</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -9,6 +9,11 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet">
-
 <link rel="shortcut icon" href="#" />
-
+<link rel="shortcut icon" href="{{ asset('images/myfavicon.png') }}" />
+<!-- Open Graph Meta Tags -->
+<meta property="og:title" content="SkatesSpots">
+<meta property="og:description" content="Discover and share the best skate spots around the world.">
+<meta property="og:image" content="{{ asset('images/myfavicon.png') }}">
+<meta property="og:url" content="{{ url()->current() }}">
+<meta property="og:type" content="website">
